6249bc6c47ec19a810257779f452fbf88a3568b6
fatevil <marekoka@seznam.cz>
2017-03-30 17:20:34 +0300

    Server routing adjusted.

--- server.js
+++ server.js
@@ -2,5 +2,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/fonts', express.static(__dirname + 'public/fonts'));
+
 
 app.listen(3000);
