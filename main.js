0067f898cc3d23100eceeb27c9dcaafe011c14f0
fatevil <marekoka@seznam.cz>
2017-03-30 16:21:06 +0300

    Simple node.js server created.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
  "use strict";
  const fetchData = () => {
 
-     return fetch('http://localhost:8080/data/data.json').then((response) => {
+     return $.getJSON('http://localhost:8080/data/data.json').then((response) => {
          // Yay, `j` is a JavaScript object
          return response;
      });
