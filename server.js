64f64a14c981609baecea4107060df42e4015f3a
fatevil <marekoka@seznam.cz>
2017-03-30 20:55:30 +0300

    Form for creating designed.

--- server.js
+++ server.js
@@ -2,9 +2,5 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
-app.use('/css', express.static('public/css'));
-app.use('/js', express.static('public/js'));
-app.use('/fonts', express.static('public/fonts'));
-
 
 app.listen(3000);
