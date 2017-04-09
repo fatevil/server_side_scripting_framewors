657fe36d31ee0d5fa1d0115d39d248d744ee0039
fatevil <marekoka@seznam.cz>
2017-04-09 23:28:29 +0300

    API documentation added to some routes.

--- server.js
+++ server.js
@@ -41,6 +41,20 @@ const observation = mongoose.Schema({
 
 const Observation = mongoose.model('Observation', observation);
 
+
+/**
+ * @api {post} /api/create Create new observation.
+ * @apiName CreateObservation
+ * @apiGroup Observation
+ * @apiDescription Create new observation with parameters in req body. 
+ *  Transforms image to thumbnail image and 768x720 image, saves then in image directory.
+ *  Should contain:
+ *      - category
+ *      - title
+ *      - locationX, locationY
+ *      - file: image
+ * 
+ */
 app.post('/api/create', upload.single('image'), function(req, res, next) {
     // req.file is the `avatar` file 
     // req.body will hold the text fields, if there were any 
@@ -72,26 +86,138 @@ app.post('/api/create', upload.single('image'), function(req, res, next) {
 
 });
 
+/**
+ * @api {get} /api/read Get collection of observations in DB.
+ * @apiName ReadObservation
+ * @apiGroup Observation
+ * @apiDescription Get collections of all observations from database.
+ *
+ * @apiError ObservationsNotFound 404 No observations found.
+ */
 app.get('/api/read', function(req, res) {
     // console.log('GET read all');
     Observation.find((err, observationCollection) => {
         if (err) return console.error(err);
+        if (observationCollection.length == 0) {
+            res.statusCode = 404;
+            res.statusMessage = 'Observation with given ID not found.';
+        } else {
+            res.statusMessage = 'Ok';
+        }
         res.send(observationCollection);
     });
 });
+
+/**
+ * @api {get} /api/read/:id Get Observation by unique ID.
+ * @apiName ReadObservationById
+ * @apiGroup Observation
+ * @apiDescription Get observation with ID from parameter.
+ *
+ * @apiParam {id} unique ID of observation
+ *
+ * @apiError ObservationsNotFound 404 Observation with given ID not found.
+ */
+app.get('/api/read/:id', function(req, res) {
+    const id = req.params.id;
+    Observation.findById(id, (err, observation) => {
+        if (err) return console.error(err);
+        if (observationCollection.length == 0) {
+            res.statusCode = 404;
+            res.statusMessage = 'Observation with given ID not found.';
+        } else {
+            res.statusMessage = 'Ok';
+        }
+        res.send(observationCollection);
+    });
+});
+
+
+/**
+ * @api {get} /api/read/:property/:value Observations with filter on one atribute.
+ * @apiName ReadObservationWithFilter
+ * @apiGroup Observation
+ * @apiDescription Get observations where specified attribute contains specified string.
+ *
+ * @apiParam {property} name of attribute to be filtered
+ * @apiParam {value} value that attribute {property} is supposed to contain
+ *
+ * @apiError ObservationsNotFound  No observations with applied filter were found.
+ */
 app.get('/api/read/:property/:value', (req, res) => {
     const property = req.params.property;
     const value = req.params.value;
-    // console.log(`GET read ${property} ${value}`);
+    //console.log(`GET read ${property} ${value}`);
     Observation.where(property.toLowerCase(), {
         $regex: '.*' + value + '.*'
     }).exec((err, observationCollection) => {
         if (err) return console.error(err);
+        if (observationCollection.length == 0) {
+            res.statusCode = 404;
+            res.statusMessage = 'No observations with applied filter were found.';
+        } else {
+            res.statusMessage = 'Ok';
+        }
         res.send(observationCollection);
     });
 });
 
+/**
+ * @api {get} /api/read/:category/:title/:details Observations with filter on category, title and details.
+ * @apiName ReadObservationExtendedFilter
+ * @apiGroup Observation
+ * @apiDescription Get observations with applied filter according to following. 
+ * Each parameter specifies value which attributes category, title and details  have to contain.
+ * For not aplying filter, put '-' instead. 
+ * 
+ * @apiExample {curl} Example usage - will find all observation:
+ *  -  whose category contains "Another" and details contain "Helsinki"
+ *  - there's no filter for title
+ *     curl -i http://localhost:3000/api/read/Another/-/Helsinki/
+ *
+ *
+ * @apiError ObservationsNotFound  No observations with applied filter were found.
+ */
+app.get('/api/read/:category/:title/:details', (req, res) => {
+    const category = (req.params.category != '-') ? req.params.category : '';
+    const title = (req.params.title != '-') ? req.params.title : '';
+    const details = (req.params.details != '-') ? req.params.details : '';
 
+    Observation.
+    where('category', {
+        $regex: '.*' + category + '.*'
+    }).where('title', {
+        $regex: '.*' + title + '.*'
+    }).where('details', {
+        $regex: '.*' + details + '.*'
+    }).exec((err, observationCollection) => {
+        if (err) return console.error(err);
+        if (observationCollection.length == 0) {
+            res.statusCode = 404;
+            res.statusMessage = 'Observation with given ID not found.';
+        } else {
+            res.statusMessage = 'Ok';
+        }
+        res.send(observationCollection);
+    });
+});
+
+/**
+ * @api {get} /api/read/:category/:title/:details Observations with filter on category, title and details.
+ * @apiName ReadObservationExtendedFilter
+ * @apiGroup Observation
+ * @apiDescription Get observations with applied filter according to following. 
+ * Each parameter specifies value which attributes category, title and details  have to contain.
+ * For not aplying filter, put '-' instead. 
+ * 
+ * @apiExample {curl} Example usage - will find all observation:
+ *  -  whose category contains "Another" and details contain "Helsinki"
+ *  - there's no filter for title
+ *     curl -i http://localhost:3000/api/read/Another/-/Helsinki/
+ *
+ *
+ * @apiError ObservationsNotFound  No observations with applied filter were found.
+ */
 app.delete('/api/delete/:observationId', (req, res) => {
     Observation.findById(req.params.observationId).remove().exec()
         .then(() => {
